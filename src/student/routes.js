28244e704cabe9526c96e145abde3c94152cd3f3
akglbahadr <akgl.bahadr@gmail.com>
2022-01-22 01:18:21 +0300

    routes for queries are added

--- src/student/routes.js
+++ src/student/routes.js
@@ -4,7 +4,9 @@ const controller = require("./controller");
 const router = Router();
 
 router.get("/", controller.getStudents);
-router.post("/", controller.addStudents);
+router.post("/", controller.addStudent);
 router.get("/:id", controller.getStudentsById);
+router.put("/:id", controller.updateStudentsById);
+router.delete("/:id", controller.deleteStudentsById);
 
 module.exports = router;
