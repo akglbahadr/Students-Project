9688767f3ece483dc19c45ed00ffdb0508cd8eb4
akglbahadr <akgl.bahadr@gmail.com>
2022-01-22 00:13:01 +0300

    routes are added for queries

--- src/student/routes.js
+++ src/student/routes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
+const controller = require("./controller");
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Using API router");
-});
+router.get("/", controller.getStudents);
+router.post("/", controller.addStudents);
+router.get("/:id", controller.getStudentsById);
 
 module.exports = router;
